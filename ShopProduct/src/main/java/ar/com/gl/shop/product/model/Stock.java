b7664ad895c8adbfd8ff125395ef23f69c6e6fb8
maribel <martinezmaribel1994@gmail.com>
2020-07-27 14:44:28 -0300

    model

---
+++ ShopProduct/src/main/java/ar/com/gl/shop/product/model/Stock.java
@@ -0,0 +1,27 @@
+package ar.com.gl.shop.product.model;
+
+public class Stock {
+
+	private Long id;
+	private Integer quantity;
+	private String locationCode;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public String getLocationCode() {
+		return locationCode;
+	}
+	public void setLocationCode(String locationCode) {
+		this.locationCode = locationCode;
+	}
+}
